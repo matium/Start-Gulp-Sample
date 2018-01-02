ba0501c871a451e592ba761bd982723757268c0b
matium <develop@matium.jp>
2018-01-02 19:40:26 +0900

    Compassの設定とTypeScriptのコンパイル設定

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public_html/css"
+sass_dir = "src/scss"
+images_dir = "public_html/images"
+javascripts_dir = "public_html/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
